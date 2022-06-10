f53a25912e0d334b587eb5a4747d7cd6d9d60d61
Alexey233 <71381530+Alexey233@users.noreply.github.com>
2022-06-11 00:40:16 +0300

    Закончил настройку курсы-группы

--- Database 201-721/Controllers/GroupController.cs
+++ Database 201-721/Controllers/GroupController.cs
@@ -2,6 +2,7 @@
 using Database_201_721.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Database_201_721.Controllers
 {
@@ -62,7 +63,7 @@ namespace Database_201_721.Controllers
             return RedirectToAction("GroupList");
         }
 
-
+        [Authorize(Roles = "admin")]
         public IActionResult StudentsInGroup(int id)
         {
             var allStudentsAndGroup = new GroupAndStudentsViewModel
@@ -73,11 +74,50 @@ namespace Database_201_721.Controllers
             return View(allStudentsAndGroup);
         }
 
+        [Authorize(Roles = "admin")]
         public IActionResult ListCoursesInGroupe(int id)
         {
-            return View();
+            var allGroup = _applicationContext.Groups.Include(c => c.Courses).ToList();
+            var group = allGroup.FirstOrDefault(x => x.Id == id);
+
+            var coursesList = new GroupAndCourseViewModel { Courses = group.Courses, Group = group };
+
+            return View(coursesList);
         }
 
+
+
+        [Authorize(Roles = "admin")]
         
+        public IActionResult AddCoursesInGroup(int id)
+        {
+            var courseList = _applicationContext.Courses.ToList();
+            var group = _applicationContext.Groups.FirstOrDefault(x => x.Id == id);
+
+            var allCourses = new GroupAndCourseViewModel
+            {
+                Courses = courseList,
+                Group = group
+            };
+            return View(allCourses);
+        }
+
+
+        [Authorize(Roles = "admin")]
+        public IActionResult AddCourseInGroup(int courseId, int groupId)
+        {
+            var group = _applicationContext.Groups.FirstOrDefault(x => x.Id == groupId);
+            var course = _applicationContext.Courses.FirstOrDefault(x => x.Id == courseId);
+
+            group.Courses.Add(course);
+            course.Groups.Add(group);
+            _applicationContext.Groups.Update(group);
+ 
+
+            _applicationContext.SaveChanges();
+            return RedirectToAction("ListCoursesInGroupe", "Group", new { id = group.Id });
+        }
+
+
     }
 }
